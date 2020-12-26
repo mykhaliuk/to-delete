f4f0bfdbd72b022f3f41acbd7f03998f5e72621b
Mykhaliuk Vladimir <mykhaliuk@me.com>
2020-12-26 12:51:33 +0100

    init

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import logo from "./logo.svg";
+
+import Card, {
+  CardPrimaryContent,
+  CardMedia,
+  CardActions,
+  CardActionButtons,
+  CardActionIcons,
+} from "@material/react-card";
+
+import "@material/react-checkbox/dist/checkbox.css";
+import "@material/react-icon-button/dist/icon-button.css";
+import "@material/react-list/dist/list.css";
+import "@material/react-text-field/dist/text-field.css";
+import "@material/react-button/dist/button.css";
+import "@material/react-card/dist/card.css";
+
+import "src/App-style.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <Card>
+        <div>
+          <p>sldf</p>
+        </div>
+      </Card>
+    </div>
+  );
+}
+
+export default App;
